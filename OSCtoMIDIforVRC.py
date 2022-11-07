fa3f8fec0eb349012b42e92b44c8b9e028fe4e50
jpnatu <jpnatu2569@gmail.com>
2022-11-07 15:30:43 +0900

    successful making port change widget

--- OSCtoMIDIforVRC.py
+++ OSCtoMIDIforVRC.py
@@ -1,63 +1,66 @@
+from modulefinder import Module
 import tkinter as tk
-from tkinter import CENTER, NW, Button, ttk
+from tkinter import CENTER, NW, RIGHT, Button, ttk
+from tkinter.messagebox import showinfo
+from unittest import mock
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
 import threading
 import logic
 
-ip, port = "127.0.0.1", 9001
-
-
 def main():
-    th1 = threading.Thread(target=osc_serve)
-    th1.start()
-    win = tk.Tk()
-    app = Frame1(master=win)
+    # いったんOSC無効化
+    # th1 = threading.Thread(target=osc_serve)
+    # th1.start()
+    root = tk.Tk()
+    app = Application(master=root)
     app.mainloop()
 
-# https://denno-sekai.com/tkinter-frame/
-# Frame1クラスの作成
+# Tk.Frameを継承したApplicationクラスの作成
+class Application(tk.Frame):
+    def __init__(self, master=None):
+        super().__init__(master, width=400, height=300)
 
-
-class Frame1(tk.Frame):
-    def __init__(self, master):  # コンストラクタを定義
-        super().__init__(master)  # 継承元クラス（tk.Frame）のコンストラクタを呼び出し
-
-        # ウィンドウの設定
-        master.title('OSCtoMIDIforVRC')
-        master.geometry("800x500")
-
-        # フレームの設定
-        self.config(bg="#4682b4")  # 背景色を指定
+        #ウィンドウの設定
+        self.master.title('OSCtoMIDIforVRC')
         self.pack()
+        self.create_widget()
 
-        keyframe = tk.Frame(master)
-        keyframe.pack(fill=tk.BOTH, anchor=tk.CENTER)
+    #ウィジェットの作成
+    def create_widget(self):
+        
+        # ポート選択時のイベント
+        def selected(self):
+            # 選択インデックス取得
+            num = lb1.curselection()
+            # ポート変更
+            logic.portschange(lb1.index(num))
+            # デバッグ
+            print(lb1.get(num))
+        
+        # frame1と出力先選択用のlistbox作成
+        frame1 = tk.Frame(self.master)
 
-        # keyBK = tk.Frame(keyframe, relief="ridge")
-        # notelist = ("C", "D", "F", "G", "A")
-        # for note in notelist:
-        #     btn = tk.Button(keyBK, background="black", fg="White",
-        #                     text=note, font=("MSゴシック", "20"))
-        #     btn.pack(side=tk.LEFT)
-        # keyBK.pack(anchor=tk.CENTER)
+        # listboxの中身（ポート一覧）
+        listvalue = logic.portscheck()
+        var = tk.StringVar(value=listvalue)
+        lb1 = tk.Listbox(frame1,width=30,listvariable=var)
+        
+        lb1.pack()
+        frame1.pack()
+        lb1.bind('<<ListboxSelect>>', selected)
+        
 
-        # keyWH = tk.Frame(keyframe, relief="ridge")
-        # notelist = ("C", "D", "E", "F", "G", "A", "B")
-        # for note in notelist:
-        #     btn = tk.Button(keyWH, text=note, font=("MSゴシック", "20"))
-        #     btn.pack(side=tk.LEFT)
-        # keyWH.pack(anchor=tk.CENTER)
-
-
+        
+# VRCからのOSCを受け取る
 def osc_serve():
+    ip, port = "127.0.0.1", 9001
     dispatcher = Dispatcher()
     dispatcher.map("/avatar/parameters/GestureRight", logic.midiR)
     dispatcher.map("/avatar/parameters/GestureLeft", logic.midiL)
     server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
     print("Serving on {}".format(server.server_address))
     server.serve_forever()
-
 
 if __name__ == "__main__":
     main()
