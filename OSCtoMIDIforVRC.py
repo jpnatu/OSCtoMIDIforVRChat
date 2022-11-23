ca1b72381874b0d3f86c6e35ec78bcff883d3f3a
jpnatu <jpnatu2569@gmail.com>
2022-11-23 09:36:40 +0900

    サーバー処理をThreadingに変更、その他UIの改善

--- OSCtoMIDIforVRC.py
+++ OSCtoMIDIforVRC.py
@@ -1,23 +1,19 @@
 import tkinter as tk
-import multiprocessing
+import threading
 from pythonosc import osc_server
 from pythonosc.dispatcher import Dispatcher
-
 import logic
-
 
 def main():
     global oscserver
-    oscserver = multiprocessing.Process(target=osc_serve)
+    oscserver = threading.Thread(target=osc_serve,daemon=True)
     oscserver.start()
     root = tk.Tk()
-    root.minsize(height=300, width=500)
+    root.minsize(width=500, height=300)
     app = Application(master=root)
     app.mainloop()
 
 # Tk.Frameを継承したApplicationクラスの作成
-
-
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -25,19 +21,20 @@ class Application(tk.Frame):
         # ウィンドウの設定
         self.master.title('OSCtoMIDIforVRC')
         # メインフレームの配置
-        self.pack(fill=tk.BOTH)
+        self.pack(fill=tk.BOTH, expand=True)
         self.master.protocol("WM_DELETE_WINDOW", self.delete_window)
         self.create_widget()
 
     # ウィジェットの作成
     def create_widget(self):
 
-        # listboxの親としてframe1
-        frame1 = tk.Frame(self, bg="red")
+        # frame1
+        frame1 = tk.Frame(self, bg="red",background="#61a4e0")
+        frame1.pack(side=tk.LEFT, fill=tk.BOTH)
 
-        # ラベル
         label1 = tk.Label(frame1, text="Avalable Ports")
-
+        label1.pack(side=tk.TOP, padx=10,pady=10)
+        
         # ポート選択時のイベント
         def selected(self):
             # 選択インデックス取得
@@ -45,40 +42,38 @@ class Application(tk.Frame):
             # ポート変更
             logic.portschange(listbox1.index(selectedind))
 
-        # listboxの中身（ポート一覧）
+        # listbox
         listvalue = logic.portscheck()
         var = tk.StringVar(value=listvalue)
         listbox1 = tk.Listbox(frame1, selectmode="single",
                               width=30, listvariable=var)
 
-        frame2 = tk.Frame(self, bg="green")
-        label2 = tk.Label(frame2, text="Aple")
-
-        # 配置
-        frame1.pack(side=tk.LEFT, anchor=tk.W, fill=tk.Y)
-        frame2.pack(side=tk.LEFT, anchor=tk.E, fill=tk.Y)
-        label1.pack(side=tk.TOP)
-        listbox1.pack(side=tk.TOP)
+        listbox1.pack(side=tk.TOP,padx=10)
         listbox1.bind('<<ListboxSelect>>', selected)
-        label2.pack(side=tk.TOP)
+
+
+        frame2 = tk.Frame(self, bg="green")
+        frame2.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        
+        label2 = tk.Label(frame2, text="Left hand settings")
+        label2.pack(side=tk.TOP, padx=10,pady=10)
+
+        frame3 = tk.Frame(self, bg="orange")
+        frame3.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        label3 = tk.Label(frame3, text="Right hand settings")
+        label3.pack(side=tk.TOP, padx=10,pady=10)
 
     # 終了処理
     def delete_window(self):
         self.master.destroy()
-        oscserver.terminate()
 
 # VRCからのOSCを受け取るサーバー
-
-
 def osc_serve():
-    ip, port = "127.0.0.1", 9001
-    dispatcher = Dispatcher()
+    ip, port, dispatcher= "127.0.0.1", 9001, Dispatcher()
     dispatcher.map("/avatar/parameters/GestureRight", logic.midiR)
     dispatcher.map("/avatar/parameters/GestureLeft", logic.midiL)
     server = osc_server.ThreadingOSCUDPServer((ip, port), dispatcher)
-    print("Serving on {}".format(server.server_address))
     server.serve_forever()
-
 
 if __name__ == "__main__":
     main()
