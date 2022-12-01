3d4bbe93208b1e1aa7bbde104a40b41de939566c
jpnatu <jpnatu2569@gmail.com>
2022-12-01 21:42:58 +0900

    this has still bug

--- logic.py
+++ logic.py
@@ -2,23 +2,19 @@ import json
 import mido
 from mido import Message
 
-json_open = open("keylist.json", "r")
-json_load = json.load(json_open)
-print("Module_Logic has connected")
+keylist = json.load(open("keylist.json", "r"))
 ports = mido.get_output_names()
-print(mido.get_output_names())
 outport = mido.open_output(ports[0])
+
+def channel_setting(ch):
+    pg=keylist["Channel"][ch]
+    outport.send(Message("program_change",channel=ch,program=pg))
 
 def json_reload():
     json_open = open("keylist.json", "r")
     global json_load
     json_load = json.load(json_open)
     print(json_load["Righthand"])
-
-    #プログラムチェンジを設定
-    for index in range(len(json_load["Righthand"])):
-        outport.send(Message("program_change",\
-        program=json_load["Righthand"][index][3]))
 
 # ポート管理
 def portscheck():
@@ -37,7 +33,10 @@ def midiR(path, righthand=0):
     print(path, righthand, json_load["Righthand"][righthand])
     outport.send(Message(json_load["Righthand"][righthand][1],
     channel=json_load["Righthand"][righthand][2],
-    note   =json_load["Righthand"][righthand][4]))
+    note   =json_load["Righthand"][righthand][3]))
     
 def midiL(path, lefthand=0):
-    print(path, lefthand)
+    print(path, lefthand, json_load["Lefthand"][lefthand])
+    outport.send(Message(json_load["Lefthand"][lefthand][1],
+    channel=json_load["Lefthand"][lefthand][2],
+    note   =json_load["Lefthand"][lefthand][3]))
